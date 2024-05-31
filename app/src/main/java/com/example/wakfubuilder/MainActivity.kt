bda4984577c9c09435bf7cecbf5c933d0c26d5ef
Guillermo Serna <98768855+Pasternark@users.noreply.github.com>
2024-05-31 03:36:54 -0600

    v2

--- app/src/main/java/com/example/wakfubuilder/MainActivity.kt
+++ app/src/main/java/com/example/wakfubuilder/MainActivity.kt
@@ -3,26 +3,36 @@ package com.example.wakfubuilder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,18 +47,11 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.wakfubuilder.model.BottomNavItem
 import com.example.wakfubuilder.ui.theme.WakfuBuilderTheme
 
-enum class WakfuScreens(@StringRes val title: Int){
-    MainActivity(title = R.string.main),
-    Inicio(title = R.string.app_name),
-    Favoritos(title = R.string.favoritos),
-    Ajustes(title = R.string.ajustes),
-    Busqueda(title = R.string.buscar),
-    Creacion(title = R.string.nuevaBuild)
-
-}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,7 +63,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginWakfu()
+                    WakfuBuilder()
                 }
             }
         }
@@ -68,37 +71,48 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginWakfu (
-    navController: NavHostController = rememberNavController()
-){
+fun WakfuBuilder() {
+    val navController = rememberNavController()
 
     Scaffold(
-        topBar = {
-            WakfuLoginTopBar()
+        bottomBar = {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+            if (currentDestination?.route != "Login") {
+                BottomNavigationBar(navController = navController)
+            }
         }
-    ) {
-        LazyColumn(contentPadding = it) {
-
-        }
+    ) { paddingValues ->
         NavHost(
             navController = navController,
-            startDestination = WakfuScreens.MainActivity.name,
-            modifier = Modifier.padding()
-        ){
-            composable(route = WakfuScreens.MainActivity.name){
-                navController.navigate(WakfuScreens.MainActivity.name)
-            }
-            composable(route = WakfuScreens.Inicio.name){
-
-            }
+            startDestination = "Login",
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable("Login") { WakfuLogin(navController = navController) }
+            composable("Inicio") { WakfuInicio(navController = navController) }
+            composable("Favoritos") { WakfuFavoritos(navController = navController) }
+            composable("Buscar") { WakfuBusqueda(navController = navController) }
+            composable("Crear") { WakfuCreacion(navController = navController) }
+            composable("Ajustes") { WakfuAjustes(navController = navController) }
         }
+
     }
-    Column (
+
+
+}
+
+@Composable
+fun WakfuLogin(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-    ){
-        Image(painter = painterResource(id = R.drawable.wakfubuilderlogo),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.wakfubuilderlogo),
             contentDescription = "Wakfu Builder Logo",
             contentScale = ContentScale.Crop,
             modifier = Modifier
@@ -107,7 +121,8 @@ fun LoginWakfu (
                 .padding(bottom = 20.dp)
                 .padding(horizontal = 20.dp)
         )
-        Text(text = "¡Bienvenido!",
+        Text(
+            text = "¡Bienvenido!",
             modifier = Modifier
                 .padding(bottom = 20.dp),
             fontFamily = FontFamily.Monospace,
@@ -118,47 +133,210 @@ fun LoginWakfu (
             modifier = Modifier
                 .size(width = 130.dp, height = 50.dp),
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF026673)),
+                containerColor = Color(0xFF026673)
+            ),
 
             onClick = {
-                navController.navigate(WakfuScreens.Inicio.name)
+                navController.navigate("Inicio")
             }) {
-            Text(text = stringResource(R.string.buttonAcceder),
+            Text(
+                text = stringResource(R.string.buttonAcceder),
                 modifier = Modifier,
                 fontWeight = FontWeight.Bold,
                 fontSize = 20.sp
             )
         }
     }
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WakfuLoginTopBar(){
-    CenterAlignedTopAppBar(
+fun WakfuInicio(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
 
-        title = {
-            Row(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
+                title = {
 
-            }
+                    Text(
+                        text = stringResource(R.string.app_name),
+                    )
+
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.navigate("Ajustes")
+                        }
+                    ) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Settings")
+                    }
+
+
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color(0xFF026673),
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
         }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+
+        }
+    }
+    Column(
+        modifier = Modifier
+            .padding(bottom = 20.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        Button(
+            modifier = Modifier
+                .size(width = 130.dp, height = 50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF026673)
+            ),
+
+            onClick = {
+                navController.navigate("Crear")
+            }) {
+            Text(
+                text = stringResource(R.string.crearBuild),
+                modifier = Modifier,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WakfuFavoritos(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            WakfuTopBar(navController = navController, stringResource(id = R.string.favoritos))
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WakfuCreacion(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            WakfuTopBar(navController = navController, stringResource(id = R.string.crearBuild))
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WakfuAjustes(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            WakfuTopBar(navController = navController, stringResource(id = R.string.ajustes))
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WakfuBusqueda(navController: NavHostController) {
+
+    Scaffold(
+        topBar = {
+            WakfuTopBar(navController = navController, stringResource(id = R.string.buscar))
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+
+        }
+    }
+}
+
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val itemsBarNavigation = listOf(
+        BottomNavItem("Inicio", Icons.Default.Home, "Inicio"),
+        BottomNavItem("Favoritos", Icons.Default.Favorite, "Favoritos"),
+        BottomNavItem("Buscar", Icons.Default.Search, "Buscar")
+    )
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        itemsBarNavigation.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = currentDestination?.route == item.route,
+                onClick = { navController.navigate(item.route) }
+
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WakfuTopBar(navController: NavHostController, stringResource: String) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource,
+                modifier = Modifier,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp
+            )
+        },
+        navigationIcon = {
+
+            IconButton(
+                onClick = {
+                    navController.navigateUp()
+                }
+            ) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+            }
+        },
+
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color(0xFF026673),
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
     )
 }
 
 @Composable
-fun AccederInicio(){
+fun AccederInicio() {
 
 }
+
 @Preview
 @Composable
-fun LoginPreview(){
+fun LoginPreview() {
     WakfuBuilderTheme {
-        LoginWakfu()
+        WakfuBuilder()
     }
 }
---
+++ app/src/main/java/com/example/wakfubuilder/model/model.kt
@@ -0,0 +1,7 @@
+package com.example.wakfubuilder.model
+
+data class BottomNavItem(
+    val route: String,
+    val icon: androidx.compose.ui.graphics.vector.ImageVector,
+    val title: String
+)
